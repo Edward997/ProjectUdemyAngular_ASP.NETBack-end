3a7ac6e5d2f2540672bd059ecde684ffe117e40f
Edward997 <edward.navarrete7@gmail.com>
2023-07-03 17:26:39 -0600

    Paginacion WEB API

--- back-end/DTOs/GeneroCreacionDTO.cs
+++ back-end/DTOs/GeneroCreacionDTO.cs
@@ -1,6 +1,14 @@
-﻿namespace back_end.DTOs
+﻿using back_end.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace back_end.DTOs
 {
     public class GeneroCreacionDTO
     {
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50)]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
     }
 }
