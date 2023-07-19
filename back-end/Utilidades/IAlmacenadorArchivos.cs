2d6f463dd5913414fb83f4a36fb8eea524b38e14
Edward997 <edward.navarrete7@gmail.com>
2023-07-19 11:52:20 -0600

    Guardando una Imagen en Azure Storage

---
+++ back-end/Utilidades/IAlmacenadorArchivos.cs
@@ -0,0 +1,9 @@
+﻿namespace back_end.Utilidades
+{
+    public interface IAlmacenadorArchivos
+    {
+        Task BorrarArchivos(string ruta, string contenedor);
+        Task<string> EditarArchivo(string contenedor, IFormFile archivo, string ruta);
+        Task<string> GuardarArchivo(string contenedor, IFormFile archivo);
+    }
+}
