ccc421629d79f507f63766232f2b009c31774cea
Edward997 <edward.navarrete7@gmail.com>
2023-07-27 11:22:41 -0600

    Indice de Cines

--- back-end/Controllers/CinesController.cs
+++ back-end/Controllers/CinesController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using back_end.DTOs;
 using back_end.Entidades;
+using back_end.Utilidades;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace back_end.Controllers
 {
@@ -19,11 +21,35 @@ namespace back_end.Controllers
             this.mapper = mapper;
         }
 
+        [HttpGet] // api/generos
+        public async Task<ActionResult<List<CineDTO>>> Get([FromQuery] PaginacionDTO paginacionDTO)
+        {
+            var queryable = context.Cines.AsQueryable();
+            await HttpContext.InsertarParametrosPaginacionEnCabecera(queryable);
+            var cines = await queryable.OrderBy(x => x.Nombre).Paginar(paginacionDTO).ToListAsync();
+            return mapper.Map<List<CineDTO>>(cines);
+        }
+
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] CineCreacionDTO cineCreacionDTO)
         {
             var cine = mapper.Map<Cine>(cineCreacionDTO);
             context.Add(cine);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Cines.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Cine() { Id = id });
             await context.SaveChangesAsync();
             return NoContent();
         }
